a7e2e3177bd23abb24f347b356147d5bcf1466fd
Z-Kamili <zakariakamili97@gmail.com>
2021-01-06 18:47:09 +0100

    first commit

---
+++ src/employeemain/Employeemain.java
@@ -0,0 +1,10 @@
+package employeemain;
+
+public class Employeemain {
+
+	public static void main(String[] args) {
+		//diffrence entre abstract et non abstract classes c'est l'instantiation de la classe parent
+		
+	}
+
+}
